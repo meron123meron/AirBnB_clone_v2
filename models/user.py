44abb83319c19638ca497daf93e8fe65a4b96006
meron nigussie <merontafer@gmail.com>
2022-12-07 13:08:27 +0300

    task 7

--- models/user.py
+++ models/user.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
-"""This module defines a class User"""
+"""A module which defines a class User"""
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 
 
 class User(BaseModel, Base):
-    """This class defines a user by various attributes"""
+    """A class which defines a user by various attributes"""
     __tablename__ = 'users'
 
     email = Column(String(128), nullable=False)
