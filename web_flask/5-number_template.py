fd3c708ea76dc40c3621b6c6ac301b71a8de1f7c
meron <meron@example.com>
2023-01-17 03:36:43 -0800

    task 5

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -41,5 +41,11 @@ def n_text(n):
     return "{} is a number".format(n)
 
 
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def n_template(n):
+    """displays a HTML page if n is an integer"""
+    return render_template('5-number.html', n=n)
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
